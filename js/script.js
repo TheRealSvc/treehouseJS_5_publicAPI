c01688ece54ec7cee478ebd61fc41eeacccf666d
SVC <svcoelln@yahoo.de>
2021-03-22 21:41:39 +0100

    is this the way to go

--- js/script.js
+++ js/script.js
@@ -1,47 +1,99 @@
-// define global DOM references
-
+// define globals
+const singleRequest = 'https://randomuser.me/api/?results=12'
 const gallery = document.getElementById('gallery');
+const body = document.querySelector('body');
+
 
 // ------------------------------------------
-//  FETCH FUNCTIONS
+//  FETCH AND DISPLAY GALLERY  
 // ------------------------------------------
 
-/*
-The directory displays 12 users from the Random User API, and includes the following for each user:
-Employee Image
-First and Last Name
-Email
-City or location */
+function generateGalleryItem(fetchedEl) {
+    console.log(fetchedEl) ;
+    console.log(typeof(fetchedEl));
 
-function addGalleryItem(fetchedEl) {
-    myString = `<div class="card-img-container">
-                <img class="card-img" src="${ipadress}" alt="profile picture">
+    myString = `<div class="card">
+                <div class="card-img-container">
+                <img class="card-img" src="${fetchedEl.picture.medium}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
-                <p class="card-text">${emailAdress} </p>
-                <p class="card-text cap"> ${city}, ${state}</p>
+                <h3 id="name" class="card-name cap">${fetchedEl.name.first} ${fetchedEl.name.last}</h3>
+                <p class="card-text">${fetchedEl.email} </p>
+                <p class="card-text cap"> ${fetchedEl.location.city}, ${fetchedEl.location.state}</p>
+                </div> 
                 </div>`;
+    return myString
 };
-//gallery.element.insertAdjacentHTML('beforeend', 'HTML string');
 
-function reloadGallery() {
-    for(i=1;i<=12;i++) {
-        var startDiv = '<div class="card">';
-        var endDiv = '</div>';
-        fetch('https://randomuser.me/api')
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .then((data) => addGalleryItem(data))
-            .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-            });
-    }
+function generateModal(fetchedEl) {
+    console.log(fetchedEl) ;
+    myString = `<div class="modal-container">
+                <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                <img class="modal-img" src="${fetchedEl.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap"> ${fetchedEl.name.first} ${fetchedEl.name.last}</h3>
+                <p class="modal-text">${fetchedEl.email}</p>
+                <p class="modal-text cap">${fetchedEl.location.city}</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555 ${fetchedEl.cell}</p>
+                <p class="modal-text"> ${fetchedEl.location.street.number} ${fetchedEl.location.street.name} ,${fetchedEl.location.city}, OR ${fetchedEl.location.postcode}</p>
+                <p class="modal-text">Birthday: ${fetchedEl.dob.date.slice(0,9)}</p>
+                </div>
+                </div>
+            </div>` ;
+    return myString
+};
+
+
+async function loadPage() {
+    const jsonDat = await fetch(singleRequest) 
+    .then(response => response.json()) 
+    //.then(x => {return(x)}) 
+    .catch(error => { console.error('the fetch operation threw the error:', error) });
+    let prepDat = await jsonDat.results.map(x => generateGalleryItem(x)) ; 
+    gallery.insertAdjacentHTML('beforeend', prepDat.join('')) ;
+    return await jsonDat ;
+} 
+
+staffDat = loadPage() ;  
+
+console.log(typeof(staffDat)) ;
+async function addListeners(jsonDat) {    
+    document.addEventListener('click',  async (e) => {
+        if (e.target.classList.contains('card')) {
+            console.log(`hier1: ${e.target}`) ;
+            selectedEmail = e.target.getElementsByClassName('card-text')[0].innerText ;
+            console.log(selectedEmail) ;
+            jsonDat.then(x => console.log(x))
+        //body.innerHTML += generateModal(jsonDat.results[1]) ;      
+   }
+  })
 }
 
+addListeners(staffDat) ;
 
-var debuggy = fetch('https://randomuser.me/api')
-.then(response => response.json())
-.then(data => console.log(data));
 
-testObj = {"results":[{"gender":"male","name":{"title":"Mr","first":"Marto","last":"Fernandes"},"location":{"street":{"number":2754,"name":"Rua da Paz "},"city":"Marília","state":"Mato Grosso","country":"Brazil","postcode":97443,"coordinates":{"latitude":"-79.0964","longitude":"-118.4819"},"timezone":{"offset":"+5:00","description":"Ekaterinburg, Islamabad, Karachi, Tashkent"}},"email":"marto.fernandes@example.com","login":{"uuid":"a6a4386f-abc2-4804-b899-9a250fcd1bc2","username":"organicmouse658","password":"lori","salt":"OTVkGar0","md5":"db77f0892c24646e530c2f0f52ebe3ef","sha1":"c4d1be1188bdc708762352c9277b7a64b7afa2d2","sha256":"7f94308435411270b692fa9bdeab07607f62a0edcda569d932a8793a8e40ad95"},"dob":{"date":"1979-03-25T04:44:49.057Z","age":42},"registered":{"date":"2004-10-01T17:18:16.293Z","age":17},"phone":"(09) 7807-7188","cell":"(61) 3117-0408","id":{"name":"","value":null},"picture":{"large":"https://randomuser.me/api/portraits/men/33.jpg","medium":"https://randomuser.me/api/portraits/med/men/33.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/33.jpg"},"nat":"BR"}],"info":{"seed":"7b16a0a7b90d0011","results":1,"page":1,"version":"1.3"}}
+/*
+
+function fetchData(url) {
+  return fetch(url)
+           .then(checkStatus)  
+           .then(res => res.json())
+           .catch(error => console.log('Looks like there was a problem!', error))
+}
+
+Promise.all([
+  fetchData('https://dog.ceo/api/breeds/list'),
+  fetchData('https://dog.ceo/api/breeds/image/random')  
+])
+.then(data => {
+  const breedList = data[0].message;
+  const randomImage = data[1].message;
+  
+  generateOptions(breedList);
+  generateImage(randomImage);
+})
+
+
+*/
