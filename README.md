d7397737b942d84c3d131ed977eff0e8f52a0496
SVC <svcoelln@yahoo.de>
2021-03-24 11:37:26 +0100

    v1 to review

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # treehouseJS_5_publicAPI
+
+## Tested with ...
+Tested with Chrome Version 89.0.4389.90, Firefox Version 86.0.1 and Edge 89.0.774.57
+Chrome: works as expected
+Firefox: works nicely except refreshing the page. I could'nt find a fix for that. However a workaround is to press cmd+F5 to refresh.
+Edge: works as expected
+
+## Style changes in css
+body backround color set to dark grey
+card background set to light grey
+header title color set to (close to) white
+
+## exceed 
+search field works dynamic by using a keyup listener 
+removed magnifying glass for aestethic reasons. It also would have no function anyways.
+the next and prev buttons have "cycle-through" behavior. E.g. after the last person it starts from the first person. 
+
+## other 
+staff number is parametrizable in first line of script.js
+
--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ const gallery = document.getElementById('gallery') ;
 const body = document.querySelector('body') ;
 const search = document.querySelector('.search-container')
 
+
+
 /**
  *  render search container
  */
 function createSearch() {
   myString= `<form action="#" method="get">
   <input type="search" id="search-input" class="search-input" placeholder="Search...">
-  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
   </form>`
   return myString
 }
+
 
 /**
  * @param fetchedEl is the json object to reference to peoples data in the response body  
@@ -53,97 +55,139 @@ function generateModal(fetchedEl) {
                 <p class="modal-text">${fetchedEl.email}</p>
                 <p class="modal-text cap">${fetchedEl.location.city}</p>
                 <hr>
-                <p class="modal-text">(555) 555-5555 ${fetchedEl.cell}</p>
-                <p class="modal-text"> ${fetchedEl.location.street.number} ${fetchedEl.location.street.name} ,${fetchedEl.location.city}, OR ${fetchedEl.location.postcode}</p>
-                <p class="modal-text">Birthday: ${fetchedEl.dob.date.slice(0,9)}</p>
+                <p class="modal-text"> ${parsePhone(fetchedEl.cell)} </p>
+                <p class="modal-text"> ${fetchedEl.location.street.number} ${fetchedEl.location.street.name}, ${fetchedEl.location.city}, ${fetchedEl.location.postcode}</p>
+                <p class="modal-text">Birthday: ${parseBirthday(fetchedEl.dob.date.slice(0,10))}</p>
                 </div>
+                </div>
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
             </div>` ;
     return myString
 };
 
+
+/**
+ * Main fetch and parse to json
+ */
 async function loadPage() {
     const jsonDat = fetch(singleRequest) 
     .then(response => response.json()) 
     return jsonDat ; 
 }
+
+// add search field 
 search.insertAdjacentHTML('beforeend',createSearch()) ;
+
+// create Promise for getting json data from API  
 jsonDat = loadPage()
 
-// this is to decouple 
+// displaying of the staff in the gallery and registering of eventListeners
 const jsonData = Promise.resolve(jsonDat);
-    jsonData.then(x => eventHandlerFun(x)) 
+    jsonData.then(x => eventHandlerFun1(x)) 
+    .then(x => eventHandlerFun2(x)) 
     .then(x => x.results.map(x => generateGalleryItem(x)))
     .then(y => gallery.insertAdjacentHTML('beforeend', y.join('')))
-    .catch(error => { console.error('the fetch operation threw the error:', error) });
-    //let prepDat = await jsonDat.results.map(x => generateGalleryItem(x)) ; 
-    //gallery.insertAdjacentHTML('beforeend', prepDat.join('')) ;
-    //return jsonDat ;
-//} 
+    .catch(error => { console.error('the fetch operation threw the error:', error) }) ;
+
 
 /**
- * This function acts as a eventHandler to handle all relevant events 
- * @param {*} jsonDat this is only passed through 
- * @returns jsonDat unchanged
+ * This function acts as a eventHandler to handle "gallery and modal" events 
+ * @param {Promise} a is passed through 
+ * @returns a 
  */
-function eventHandlerFun(jsonDat) {    
-    document.addEventListener('click',  (e) => {
+function eventHandlerFun1(a) {    
+    document.addEventListener('click', (e) => {
         console.log(e.target.className) ;
-        galleryClick = /card|card-img|card-name|card-name cap|card-image-container|card-text cap/.test(e.target.className) ; 
-        modalClick = /modal-text|modal-name cap|modal-text cap|modal-img|modal-container|modal-info-container/.test(e.target.className) || e.target.textContent==="X";  
+        galleryClick = /card-img|card-name|card-image-container|card-text/.test(e.target.className) ; 
+        modalClick = /modal-close-btn|modal-text|modal-name|modal-img|modal-container|modal-info-container/.test(e.target.className) || e.target.textContent==="X";  
+        nextClick = /modal-next btn/.test(e.target.className) ;
+        prevClick = /modal-prev btn/.test(e.target.className) ;
         
         if (galleryClick) {
             selectedEmail = e.target.parentNode.parentNode.querySelector('.card-text').innerText ;
             console.log(selectedEmail) ;
-            var a = Promise.resolve(jsonDat) ;
-            a.then(x => {
+            Promise.resolve(a).then(x => {
               for (i=0; i < numStaff ; i++) {
                   if (x.results[i].email === selectedEmail) {
-                    body.innerHTML += generateModal(x.results[i]) ;   
+                    body.insertAdjacentHTML('beforeend', generateModal(x.results[i])) ;   
                   }
               }      
             })     
         } else if (modalClick)  {
               body.lastElementChild.remove() ;
-        } 
-  })
+        } else if (nextClick) {
+              var actualEmail = e.target.parentNode.parentNode.querySelector(".modal-text").innerText
+              body.lastElementChild.remove() ;
+              Promise.resolve(a).then(x => {
+                for (i=0; i < numStaff ; i++) {
+                    if (x.results[i].email === actualEmail) {
+                      if (i === numStaff-1) { 
+                        body.insertAdjacentHTML('beforeend', generateModal(x.results[0])) ; // circle around 
+                      } else { 
+                        body.insertAdjacentHTML('beforeend', generateModal(x.results[i+1])) ;   
+                    }
+                  }
+                }})      
+        } else if (prevClick) {
+          var actualEmail = e.target.parentNode.parentNode.querySelector(".modal-text").innerText
+          body.lastElementChild.remove() ;
+          Promise.resolve(a).then(x => {
+            for (i=0; i < numStaff ; i++) {
+                if (x.results[i].email === actualEmail) {
+                  if (i === 0) { 
+                    body.insertAdjacentHTML('beforeend', generateModal(x.results[numStaff-1])) ; // circle around 
+                  } else { 
+                    body.insertAdjacentHTML('beforeend', generateModal(x.results[i-1])) ;   
+                }
+              }
+            }})      
+        }
+  }, false)
+return a
+}
 
+/**
+ * This function handles "search events"
+ * @param {Promise} a is passed through 
+ * @returns a 
+ */
+function eventHandlerFun2(a) { 
   search.addEventListener('keyup', (e) => {
     let filterText = e.target.value.toLowerCase() ;
     gallery.innerHTML = '' ;
-    var a = Promise.resolve(jsonDat) ;
-    a.then(x => {
+    Promise.resolve(a).then(x => {
       for (i=0; i < numStaff ; i++) {
-        if (x.results[i].name.last.toLowerCase().startsWith(filterText)) {
-          gallery.innerHTML += generateGalleryItem(x.results[i]) ;   
+        if (x.results[i].name.last.toLowerCase().startsWith(filterText) || x.results[i].name.first.toLowerCase().startsWith(filterText)) {
+          gallery.insertAdjacentHTML('beforeend',generateGalleryItem(x.results[i])) ;   
         }
       }      
     })
-  })           
-  return jsonDat;
+  },false)           
+  return a;
 }
 
-jsonDat = loadPage() ;
-/* jsonDat.then(x => addSearchListener(x))
+//jsonDat = loadPage() ; 
 
-function addSearchListener(jsonIn) {
-search.addEventListener('keyup', (e) => {
-  console.log(e.target) ;
-  let filterText = e.target.value.toLowerCase() ;
-  console.log(filterText)
-  body.innerHTML = '' ;
-  for (i=0; i < numStaff ; i++) {
-    if (jsonIn.results[i].name.last.toLowerCase().startsWith(filterText)) {
-      body.innerHTML += generateModal(jsonIn.results[i]) ;   
-    }
-  }      
- // var a = Promise.resolve(jsonDat) ;
- // a.then(x => x.results.filter(y => y.name.last.toLowerCase().startsWith(searchText)))
- // a.then(x => {return(x)}) ;
-})
-}
 
-/* To Do's 
-NOTE: The formatting of the Cell Number should be (XXX) XXX-XXXX and the formatting of 
-the Birthday should be MM/DD/YYYY.
-*/
+
+
+// Helper functions ----------------------
+
+function parseBirthday(datestring) {
+  let normalized  = datestring.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)
+  normalized = normalized[0].replace(/-/g,'');
+  return normalized.replace(/([0-9]{4})([0-9]{2})([0-9]{2})/, '$2-$3-$1')
+  } 
+  
+function parsePhone(phonestring) {
+  phonestring = phonestring.replace(/[^\d]/g, "");
+  if (phonestring.length == 10) {
+    return phonestring.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+  } else {
+    return phonestring //leave as is in case length doesn't match 
+  }
+} 
+
